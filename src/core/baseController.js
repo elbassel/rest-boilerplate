736acd14363cbf6b0de43535bfd31f23d0da46c9
ahmed_elbassel <elbassel.n13@gmail.com>
2021-02-01 17:33:04 +0200

    Add transform toDate and instanceOf keywords validation

--- src/core/baseController.js
+++ src/core/baseController.js
@@ -5,66 +5,61 @@ const authorization = require('./authorization');
 const { UnexpectedError } = require('../common/errors');
 
 class BaseController {
-    constructor(config) {
-        const {
-            name, path, service, routes
-        } = config;
+  constructor(config) {
+    const {
+      name, path, service, routes,
+    } = config;
 
-        this.name = name;
-        this.path = path;
-        this.service = service;
-        this.routes = routes || [];
+    this.name = name;
+    this.path = path;
+    this.service = service;
+    this.routes = routes || [];
+  }
+
+  static async filterBody(ctx) {
+    // Filter response if possible
+    const body = _.get(ctx.response, 'body');
+    const permission = _.get(ctx._locals, 'permission');
+
+    let path = 'body';
+    const bodyDescriptor = _.get(ctx, 'response.body._i');
+    if (bodyDescriptor) {
+      path = `body.${bodyDescriptor.dataPath}`;
+      _.unset(ctx, 'response.body._i');
     }
 
-    async _filterBody(ctx) {
-        // Filter response if possible
-        const body = _.get(ctx.response, 'body');
-        const permission = _.get(ctx._locals, 'permission');
+    if (body && permission) {
+      if (permission.attributes.length > 0 && !(permission.attributes.length === 1 && permission.attributes[0] === '*')) {
+        await authorization.filterByPermission(permission, ctx.response, path);
+      }
+    }
+  }
 
-        let path = 'body';
-        const bodyDescriptor = _.get(ctx, 'response.body._i');
-        if (bodyDescriptor) {
-            path = `body.${bodyDescriptor.dataPath}`;
-            _.unset(ctx, 'response.body._i');
-        }
+  static async beforeAction(ctx, next) {
+    // Add resource name to local variables
+    _.set(ctx, '_locals.resource', this.name);
+    await next();
+  }
 
-        if (body && permission) {
-            if (permission.attributes.length > 0 && !(permission.attributes.length === 1 && permission.attributes[0] === '*')) {
-                await authorization.filterByPermission(permission, ctx.response, path);
-            }
-        }
+  static async afterAction(ctx, next) {
+    await next();
+
+    // Filter response body based on user permission (if possible)
+    await this.filterBody(ctx);
+  }
+
+  // Context Hooks ////
+  static async authorize(ctx, access, predicate) {
+    if (_.isNil(ctx)) {
+      throw new UnexpectedError('Invalid context object passed');
     }
 
-    async beforeAction(ctx, next) {
-        // Add resource name to local variables
-        _.set(ctx, '_locals.resource', this.name);
-
-        await next();
-    }
-
-    async afterAction(ctx, next) {
-        await next();
-
-        // Filter response body based on user permission (if possible)
-        await this._filterBody(ctx);
-    }
-
-    // Context Hooks ////
-    async authorize(ctx, access, predicate) {
-        if (_.isNil(ctx)) {
-            throw new UnexpectedError('Invalid context object passed');
-        }
-
-        const { user, resource } = ctx._locals;
-        const permission = await authorization.authorize(user, resource, access, predicate);
-        // Store permission for "afterAction" to be used for filtering
-        _.set(ctx, '_locals.permission', permission);
-        await authorization.filterByPermission(permission, ctx.request, 'body');
-    }
-
-    async validate(schema, data, strict = true) {
-        validator.validate(schema, data, strict);
-    }
+    const { user, resource } = ctx._locals;
+    const permission = await authorization.authorize(user, resource, access, predicate);
+    // Store permission for "afterAction" to be used for filtering
+    _.set(ctx, '_locals.permission', permission);
+    await authorization.filterByPermission(permission, ctx.request, 'body');
+  }
 }
 
 module.exports = BaseController;
--- src/core/validation/index.js
+++ src/core/validation/index.js
@@ -1,23 +1,60 @@
-const Ajv = require('ajv');
+const { ObjectID } = require('mongodb');
+const AJV = require('ajv');
+const transformDef = require('ajv-keywords/dist/definitions/transform');
+const instanceofDef = require('ajv-keywords/dist/definitions/instanceof');
+const addFormats = require('ajv-formats');
+const keywords = require('ajv-keywords');
+
+const ajvOptions = { allErrors: true, removeAdditional: true, useDefaults: true };
+
 const { ValidationError } = require('../../common/errors');
 
-class Validator {
-    constructor() {
-        this._ajv = new Ajv();
+class Validation {
+  constructor() {
+    // eslint-disable-next-line new-cap
+    this.validator = new AJV.default(ajvOptions);
+
+    instanceofDef.CONSTRUCTORS.ObjectID = ObjectID;
+    transformDef.transform.toDate = (dateString) => {
+      if (dateString instanceof Date) return dateString;
+      return new Date(dateString);
+    };
+
+    addFormats(this.validator);
+    keywords(this.validator, ['transform', 'instanceof']);
+  }
+
+  validate(schema, data) {
+    const valid = this.validator.validate(schema, data);
+    if (!valid) {
+      throw new ValidationError(null, this.validator.errors);
     }
-
-    validate(schema, data, strict = true) {
-        const nSchema = schema;
-
-        if (!strict) {
-            delete nSchema.required;
-        }
-
-        const valid = this._ajv.validate(nSchema, data);
-        if (!valid) {
-            throw new ValidationError(null, this._ajv.errors);
-        }
-    }
+    console.log(data);
+  }
 }
 
-module.exports = new Validator();
+module.exports = new Validation();
+
+// const Ajv = require('ajv');
+// const { ValidationError } = require('../../common/errors');
+//
+// class Validator {
+//   constructor() {
+//     this.ajv = new Ajv();
+//   }
+//
+//   validate(schema, data, strict = true) {
+//     const nSchema = schema;
+//
+//     if (!strict) {
+//       delete nSchema.required;
+//     }
+//
+//     const valid = this.ajv.validate(nSchema, data);
+//     if (!valid) {
+//       throw new ValidationError(null, this.ajv.errors);
+//     }
+//   }
+// }
+//
+// module.exports = new Validator();
